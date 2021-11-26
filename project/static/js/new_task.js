eab41a639bd018ce0401499340fe6dd0cc994ae1
ivanbaug <ivsitioinfo@gmail.com>
2021-11-26 17:20:50 -0500

    js to display quick tasks

---
+++ project/static/js/new_task.js
@@ -0,0 +1,45 @@
+console.log('hi')
+
+const newTaskForm = document.getElementById('new-task-form');
+const taskInput = document.getElementById('ntask-input');
+const newTDList = document.getElementById('new-tdlist');
+const taskTemplate = document.getElementById('template-task').content;
+
+const fragment = document.createDocumentFragment();
+
+let tasks = {};
+
+newTaskForm.addEventListener('submit', e =>{
+  e.preventDefault();
+  // console.log(taskInput.value);
+  setTask(e);
+})
+
+const setTask  = e =>{
+  if (taskInput.value.trim() === ''){
+    console.log('Empty form');
+    return
+  }
+
+  const ntask = {
+    id:Date.now(), 
+    text: taskInput.value,
+    isDone: false,
+  }
+  tasks[ntask.id] = ntask;
+  newTaskForm.reset();
+  taskInput.focus();
+  displayTasks();
+}
+
+const displayTasks = () =>{
+  newTDList.innerHTML = '';
+  // console.log(taskTemplate)
+  Object.values(tasks).forEach(task => {
+
+    const clone = taskTemplate.cloneNode(true);
+    clone.querySelector('label').textContent = task.text;
+    fragment.appendChild(clone);
+  } )
+  newTDList.appendChild(fragment);
+}
